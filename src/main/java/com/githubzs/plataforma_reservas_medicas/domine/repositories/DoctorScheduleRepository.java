7f4f80957e2fc80c99a6b5fb10f149a9e52a567c
Zeroneon8 <zippyditi@gmail.com>
2026-04-05 19:36:02 -0500

    Revert "crear metodo en doctorSchedule Repo"
    This reverts commit 26b3c5016902daa5041dac802f4235674ef31d6e.
    
    Commit en capa equivocada debe revertirse

--- src/main/java/com/githubzs/plataforma_reservas_medicas/domine/repositories/DoctorScheduleRepository.java
+++ src/main/java/com/githubzs/plataforma_reservas_medicas/domine/repositories/DoctorScheduleRepository.java
@@ -12,5 +12,5 @@ import com.githubzs.plataforma_reservas_medicas.domine.entities.DoctorSchedule;
 public interface DoctorScheduleRepository extends JpaRepository<DoctorSchedule, UUID> {
 
     List<DoctorSchedule> findByDoctor_IdAndDayOfWeek(UUID doctorId, DayOfWeek dayOfWeek);
-    List<DoctorSchedule> findByDoctor_Id(UUID doctorId);
+
 }
