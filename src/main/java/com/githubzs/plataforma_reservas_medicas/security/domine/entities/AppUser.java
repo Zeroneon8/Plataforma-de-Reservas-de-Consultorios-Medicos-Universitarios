3800a15b364eec6ad14b77fa01f047f9e79d4973
Zeroneon8 <zippyditi@gmail.com>
2026-05-01 11:10:51 -0500

    Añadir Role.java y AppUser.java , actualizar versión de SpringBoot (4.0.5 -> 4.0.6)

---
+++ src/main/java/com/githubzs/plataforma_reservas_medicas/security/domine/entities/AppUser.java
@@ -0,0 +1,37 @@
+package com.githubzs.plataforma_reservas_medicas.security.domine.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(name = "document_number", nullable = false, unique = true, length = 50)
+    private String documentNumber;
+
+    @NotBlank
+    @Size(max = 128)
+    @Column(nullable = false, length = 128)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<Role> roles;
+}
---
+++ src/main/java/com/githubzs/plataforma_reservas_medicas/security/domine/entities/Role.java
@@ -0,0 +1,8 @@
+package com.githubzs.plataforma_reservas_medicas.security.domine.entities;
+
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_DOCTOR,
+    ROLE_STAFF,
+    ROLE_COORDINATOR
+}
